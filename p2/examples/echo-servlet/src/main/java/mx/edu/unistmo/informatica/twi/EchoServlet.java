5c073c8e6e0b613d0ba5f4cc3e4c092bc612046a
Ricardo E. Melchor <rmelchorv@gmail.com>
2023-04-27 10:40:20 -0600

    1st. echo-servlet

--- p2/examples/echo-servlet/src/main/java/mx/edu/unistmo/informatica/twi/EchoServlet.java
+++ p2/examples/echo-servlet/src/main/java/mx/edu/unistmo/informatica/twi/EchoServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = { "/EchoServlet" })
+@WebServlet(urlPatterns = { "/servlets/EchoServlet" })
 public class EchoServlet extends HttpServlet {
 
   @Override
@@ -17,19 +17,41 @@ public class EchoServlet extends HttpServlet {
   {
     response.setContentType("text/html");
     response.setCharacterEncoding("UTF-8");
-    
+
     PrintWriter out = response.getWriter();
+    String message = request.getParameter("message");
+
     String html = String.join("\n", "<!DOCTYPE html>",
-      "<html>",
-      "  <head>",
-      "    <meta charset=\"UTF-8\">",
-      "    <title>EchoServlet</title>",
-      "  </head>",
-      "  <body>",
-      "    <h1>EchoServlet is listening!</h1>",
-      "    <p>Message: " + request.getParameter("message") + "</p>",
-      "  </body>",
-      "</html>");
+      "<html lang=\"es\">",
+      " <head>",
+      "  <meta charset=\"UTF-8\">",
+      "  <title>EchoServlet</title>",
+      "  <style>",
+      "   .blue {",
+      "    color: blue;",
+      "   }",
+      "   .green {",
+      "    color: green;",
+      "   }",
+      "  </style>",
+      " </head>",
+      " <body>",
+      "  <h1 class=\"green\">EchoServlet</h1>",
+      "  <p>Message: <span class=\"blue\">\"" + ((message != null) ? message : "") + "\"</span></p>",
+/*      
+      "  <p>Request method: " + request.getMethod() + "</p>",
+      "  <p>Request URI: " + request.getRequestURI() + "</p>",
+      "  <p>Request protocol: " + request.getProtocol() + "</p>",
+      "  <p>Client IP: " + request.getRemoteAddr() + "</p>",
+      "  <p>Client port: " + request.getRemotePort() + "</p>",
+      "  <p>Server name: " + request.getServerName() + "</p>",
+      "  <p>Server port: " + request.getServerPort() + "</p>",
+      "  <p>Servlet name: " + getServletName() + "</p>",
+      "  <p>Servlet info: " + getServletInfo() + "</p>",
+*/
+      " </body>",
+      "</html>"
+    );
 
     out.println(html);
   }
