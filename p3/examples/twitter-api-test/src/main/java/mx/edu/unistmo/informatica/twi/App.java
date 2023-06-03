7c793faa97624c22801be31a56245a6f021140e3
Ricardo E. Melchor <rmelchorv@gmail.com>
2023-06-03 08:58:24 -0600

    Limited V1.1 Access Only

--- p3/examples/twitter-api-test/src/main/java/mx/edu/unistmo/informatica/twi/App.java
+++ p3/examples/twitter-api-test/src/main/java/mx/edu/unistmo/informatica/twi/App.java
@@ -1,20 +1,27 @@
 package mx.edu.unistmo.informatica.twi;
 
-import twitter4j.*;
+import java.util.List;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.v1.Status;
+import twitter4j.v1.User;
 
 public class App {
     public static void main(String[] args) throws TwitterException {
-        var twitter = Twitter.newBuilder()
-            .oAuthConsumer("xKoTnZomFI42Zr3n8UIpaTlfI",
-                           "IXhvRIvV6gA6HorX2222ncolWfedlW1S8FrX2UgbPwvnLQKtdp")
-            .oAuthAccessToken("79032852-23ltfLT4VTslUKDxTFWf9CDHejN3lCensTjO1rhNM",
-                              "7BwhwNkQAdvLu6LnnFwTletdLqcFeVvqqiHCpYDsN5GjJ")
-            .build();
-        //twitter.v1().tweets().showStatus(0);
-        twitter.v1().tweets().updateStatus("Hello Twitter API!");
+        Twitter twitter = Twitter.getInstance();
 
-        //OAuth 2.0 Client ID and Client Secret
-        //Client ID         dFNwT01qYzVlWUcxbmltRUlBU3E6MTpjaQ
-        //Clinet Secret     gUzHo64ATTHBIcsw2rPfY32AkqjTqnS53oCi8yYvH03eLi22pc
+        User user = twitter.v1().users().verifyCredentials();
+
+        System.out.println("Loged as: " + user.getScreenName());
+        System.out.println("Username: " + user.getName());
+        System.out.println("Access level: " + user.getAccessLevel().name());
+
+        List<Status> statuses = twitter.v1().timelines().getHomeTimeline();
+
+        System.out.println("Showing home timeline...");
+        for (Status status : statuses) {
+            System.out.println(status.getUser().getName() + ": " + status.getText());
+        }
     }
 }
