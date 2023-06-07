c71b5f34ea29324cb33eb11c0b5f02113012c70a
Ricardo E. Melchor <rmelchorv@gmail.com>
2023-06-07 09:10:02 -0600

    twitter-api-sdk

--- p3/examples/twitter-api-test/src/main/java/mx/edu/unistmo/informatica/twi/App.java
+++ p3/examples/twitter-api-test/src/main/java/mx/edu/unistmo/informatica/twi/App.java
@@ -1,27 +1,66 @@
 package mx.edu.unistmo.informatica.twi;
 
-import java.util.List;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.v1.Status;
-import twitter4j.v1.User;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 
+/*
+ * Sample code to demonstrate the use of the v2 User Tweet timeline endpoint
+ * */
 public class App {
-    public static void main(String[] args) throws TwitterException {
-        Twitter twitter = Twitter.getInstance();
 
-        User user = twitter.v1().users().verifyCredentials();
+  // To set your environment variables in your terminal run the following line:
+  // export 'BEARER_TOKEN'='<your_bearer_token>'
 
-        System.out.println("Loged as: " + user.getScreenName());
-        System.out.println("Username: " + user.getName());
-        System.out.println("Access level: " + user.getAccessLevel().name());
+  public static void main(String args[]) throws IOException, URISyntaxException {
+    final String bearerToken = System.getenv("BEARER_TOKEN");
 
-        List<Status> statuses = twitter.v1().timelines().getHomeTimeline();
-
-        System.out.println("Showing home timeline...");
-        for (Status status : statuses) {
-            System.out.println(status.getUser().getName() + ": " + status.getText());
-        }
+    if (null != bearerToken) {
+      // Replace with user ID below
+      String response = getTweets("79032852", bearerToken);
+      System.out.println(response);
+    } else {
+      System.out.println("There was a problem getting your bearer token. Please make sure you set the BEARER_TOKEN environment variable");
     }
+  }
+
+  /*
+   * This method calls the v2 User Tweet timeline endpoint by user ID
+   * */
+  private static String getTweets(String userId, String bearerToken) throws IOException, URISyntaxException {
+    String tweetResponse = null;
+    HttpClient httpClient = HttpClients.custom()
+        .setDefaultRequestConfig(RequestConfig.custom()
+            .setCookieSpec(CookieSpecs.STANDARD).build())
+        .build();
+
+    URIBuilder uriBuilder = new URIBuilder(String.format("https://api.twitter.com/2/users/%s/tweets", userId));
+    ArrayList<NameValuePair> queryParameters;
+    queryParameters = new ArrayList<>();
+    queryParameters.add(new BasicNameValuePair("tweet.fields", "created_at"));
+    uriBuilder.addParameters(queryParameters);
+
+    HttpGet httpGet = new HttpGet(uriBuilder.build());
+    httpGet.setHeader("Authorization", String.format("Bearer %s", bearerToken));
+    httpGet.setHeader("Content-Type", "application/json");
+
+    HttpResponse response = httpClient.execute(httpGet);
+    HttpEntity entity = response.getEntity();
+    if (null != entity) {
+      tweetResponse = EntityUtils.toString(entity, "UTF-8");
+    }
+    return tweetResponse;
+  }
 }
