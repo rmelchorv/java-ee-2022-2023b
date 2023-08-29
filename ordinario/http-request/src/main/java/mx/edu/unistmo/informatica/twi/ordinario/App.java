3f11587113e70f5dfe1a4629d39c217ae76a65e3
Ricardo E. Melchor <rmelchorv@gmail.com>
2023-08-29 15:58:16 -0600

    Test!

---
+++ ordinario/http-request/src/main/java/mx/edu/unistmo/informatica/twi/ordinario/App.java
@@ -0,0 +1,13 @@
+package mx.edu.unistmo.informatica.twi.ordinario;
+
+/**
+ * HttpRequest test/example using core classes or frameworks
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
